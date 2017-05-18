55f690f80b332392702d2c1f84d253b432cfbe33
dongmingjuan <d387918156@gmail.com>
2017-05-18 12:03:35 +0800

    get the css that influences the picture effect i want

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'welcomes#index'
+  root 'welcomes#index'
   resources :welcomes1
   resources :welcomes2
   resources :welcomes3
-  root 'welcomes3#index'
+  # root 'welcomes3#index'
 end
